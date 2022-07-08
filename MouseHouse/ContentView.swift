73b04faf361b3d86fa7d947d118471bad4d738bc
Isaac Halvorson <hello@hisaac.net>
2022-07-08 14:48:08 -0500

    Initial Commit

---
+++ MouseHouse/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MouseHouse
+//
+//  Created by Isaac Halvorson on 7/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MouseHouse/MouseHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseHouseApp.swift
+//  MouseHouse
+//
+//  Created by Isaac Halvorson on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MouseHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
